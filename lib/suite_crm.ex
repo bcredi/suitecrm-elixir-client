430f247af888791da3ed2a9ae72f961ab244ba13
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2018-11-21 15:01:06 -0200

    First commit of suite crm rest client

---
+++ lib/suite_crm.ex
@@ -0,0 +1,18 @@
+defmodule SuiteCrm do
+  @moduledoc """
+  Documentation for SuiteCrm.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SuiteCrm.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/suite_crm_test.exs
@@ -0,0 +1,8 @@
+defmodule SuiteCrmTest do
+  use ExUnit.Case
+  doctest SuiteCrm
+
+  test "greets the world" do
+    assert SuiteCrm.hello() == :world
+  end
+end
