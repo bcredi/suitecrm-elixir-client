227c967bc22f563f6154f19ce16cb0beec171fb3
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2018-12-12 14:19:29 -0200

    Upgrade lib version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SuiteCrm.MixProject do
   def project do
     [
       app: :suite_crm,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps()
