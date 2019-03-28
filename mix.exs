c276bc99d8998b12b72861b8fc8adae2509621fb
Mateus Rambo Strey <mateus.rstrey@gmail.com>
2019-03-28 15:56:25 -0300

    increase lib version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SuiteCrm.MixProject do
   def project do
     [
       app: :suite_crm,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps()
